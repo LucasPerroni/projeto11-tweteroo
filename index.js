99f6abbd79f7b79e658d3060b1945d0c98f443c3
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-04-24 17:22:20 -0300

    post 'sign-up'

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const users = []
+
 app.post("/sign-up", (req, res) => {
-    res.send("Hello world!")
+    const body = req.body
+    if (body.username.length >= 2 && isValidHttpUrl(body.avatar)) {
+        // validate data
+        users.push({
+            username: body.username,
+            avatar: body.avatar,
+        })
+        res.send("OK")
+    }
+    res.send("Invalid data")
 })
 
 app.get("/tweets", (req, res) => {
@@ -18,3 +29,16 @@ app.post("/tweets", (req, res) => {
 })
 
 app.listen(5000)
+
+// FUNCTIONS
+function isValidHttpUrl(string) {
+    let url
+
+    try {
+        url = new URL(string)
+    } catch (_) {
+        return false
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:"
+}
