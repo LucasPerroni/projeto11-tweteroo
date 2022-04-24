acf9d0f410fea1ebceced300111f7c395f923e07
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-04-24 18:00:32 -0300

    get and post 'tweets'

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ app.use(cors())
 app.use(express.json())
 
 const users = []
+const tweets = []
 
 app.post("/sign-up", (req, res) => {
     const body = req.body
+    // validate data
     if (body.username.length >= 2 && isValidHttpUrl(body.avatar)) {
-        // validate data
         users.push({
             username: body.username,
             avatar: body.avatar,
@@ -21,11 +22,18 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.get("/tweets", (req, res) => {
-    res.send("Hello world!")
+    res.send(tweets.slice(-10))
 })
 
 app.post("/tweets", (req, res) => {
-    res.send("Hello world!")
+    const body = req.body
+    const user = users.find((user) => user.username === body.username) // get user data
+    tweets.push({
+        username: user.username,
+        avatar: user.avatar,
+        tweet: body.tweet,
+    })
+    res.send("OK")
 })
 
 app.listen(5000)
